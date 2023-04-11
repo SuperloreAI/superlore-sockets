6e02b6ec1c50be2ffed33e9706509aa90c84c07e
0xterran <96885027+0xterran@users.noreply.github.com>
2023-04-11 10:15:24 +0700

    setup websockets server

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+import dotenv from "dotenv";
+import cors from "cors";
+dotenv.config();
+
+const FRONTEND_DOMAIN = process.env.FRONTEND_DOMAIN;
+
+console.log(`Frontend domain = ${FRONTEND_DOMAIN}`);
+
+const corsOptions = {
+  origin: FRONTEND_DOMAIN,
+  methods: ["GET", "POST"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+const app = express();
+app.use(cors(corsOptions));
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: corsOptions,
+});
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Websockets for Superlore");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`listening on *:${PORT}`);
+});
